0fa6fc1587ca8a63fa52fc2fcffa255650e4d8f8
nktauserum <auserum@proton.me>
2026-01-14 13:43:57 +0300

    Dynamic request_buffer

--- src/request.c
+++ src/request.c
@@ -13,6 +13,7 @@ regex_t regex;
 struct request_buffer {
     char *buf;
     size_t capacity;
+    size_t total_read;
 };
 
 void init_buffer(struct request_buffer *r_buf) {
@@ -28,72 +29,72 @@ void handle_request(int *clientfd) {
     struct request_buffer buffer;
     init_buffer(&buffer);
     
-    ssize_t bytes_received = recv(*clientfd, buffer.buf, 1024, 0);    
-    if (bytes_received > 0) {
-        regcomp(&regex, "GET /([^ ]*)*", REG_EXTENDED);
-        regmatch_t ms[2];
-
-        if (regexec(&regex, buffer.buf, 2, ms, 0) < 0) {
-            printf("ERROR: regexp executing failed");
-            goto cleanup;
+    ssize_t bytes_read;
+    while ((bytes_read = recv(*clientfd, buffer.buf + buffer.total_read, buffer.capacity - buffer.total_read - 1, 0)) > 0) {
+        buffer.total_read += bytes_read;
+        
+        if (buffer.total_read >= buffer.capacity - 1) {
+            buffer.capacity *= 2;
+            char *temp = realloc(buffer.buf, buffer.capacity);
+            if (!temp) {
+                fprintf(stderr, "ERROR: realloc request_buffer\n");
+                break;
+            }
+            buffer.buf = temp;
         }
-        buffer.buf[ms[1].rm_eo] = '\0';
-
-        printf("INFO: %s\n", buffer.buf);
-
-        char *status_code = malloc(128 * sizeof(char));
-        if (!status_code) {
-            goto cleanup;
-        }
-
-        char *response_body = malloc(2048 * sizeof(char));
-        if (!response_body) {
-            free(status_code);
-            goto cleanup;
-        }
-
-        char *mime_type = malloc(256 * sizeof(char));
-        if (!mime_type) {
-            free(status_code);
-            free(response_body);
-            goto cleanup;
-        }
-
-        Response w = {
-            .mime_type = mime_type,
-            .response_body = response_body,
-            .status_code = status_code,
-        };
-
-        Request r = {.path = buffer.buf};
-
-        handle_route(&w, r);
-
-        char *header = (char*)malloc(2048 * sizeof(char));
-        if (!header) {
-            free(status_code);
-            free(mime_type);
-            free(response_body);
-            goto cleanup;
-        }
-
-        snprintf(header, 2048, 
-            "HTTP/1.1 %s\r\n"
-            "Content-Type: %s\r\n"
-            "Content-Length: %zu\r\n\r\n",
-
-            w.status_code, w.mime_type, w.body_size
-        );
-
-        send(*clientfd, header, strlen(header), 0);
-        send(*clientfd, w.response_body, w.body_size, 0);
-
-        free(header);
-        free(w.response_body);
-        free(w.mime_type);
-        free(w.status_code);
     }
 
+    char *status_code = malloc(128 * sizeof(char));
+    if (!status_code) {
+        goto cleanup;
+    }
+
+    char *response_body = malloc(2048 * sizeof(char));
+    if (!response_body) {
+        free(status_code);
+        goto cleanup;
+    }
+
+    char *mime_type = malloc(256 * sizeof(char));
+    if (!mime_type) {
+        free(status_code);
+        free(response_body);
+        goto cleanup;
+    }
+
+    Response w = {
+        .mime_type = mime_type,
+        .response_body = response_body,
+        .status_code = status_code,
+    };
+
+    Request r = {.path = buffer.buf};
+
+    handle_route(&w, r);
+
+    char *header = (char*)malloc(2048 * sizeof(char));
+    if (!header) {
+        free(status_code);
+        free(mime_type);
+        free(response_body);
+        goto cleanup;
+    }
+
+    snprintf(header, 2048, 
+        "HTTP/1.1 %s\r\n"
+        "Content-Type: %s\r\n"
+        "Content-Length: %zu\r\n\r\n",
+
+        w.status_code, w.mime_type, w.body_size
+    );
+
+    send(*clientfd, header, strlen(header), 0);
+    send(*clientfd, w.response_body, w.body_size, 0);
+
+    free(header);
+    free(w.response_body);
+    free(w.mime_type);
+    free(w.status_code);
 cleanup:
     clear_buffer(&buffer);
 }
