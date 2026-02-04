70b1827b842bd3ffd5e4b541da8ccf659f5fde96
nktauserum <auserum@proton.me>
2026-02-04 16:10:59 +0300

    Simplify the main function

--- src/main.c
+++ src/main.c
@@ -11,58 +11,20 @@
 
 #define PORT 5000
 
-struct Server {
+struct {
     int port;
     int server_fd;
     struct sockaddr_in server_addr;
-};
+} server;
 
-struct Server server;
-
-struct Server* init(int port) {
-    if ((server.server_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
-        perror("ERROR: socket failed\n");
-        return NULL;
-    }
-    
-    int opt = 1;
-    if (setsockopt(server.server_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt)) < 0) {
-        perror("ERROR: setsockopt failed\n");
-        close(server.server_fd);
-        return NULL;
-    }
-
-    server.server_addr.sin_family = AF_INET;
-    server.server_addr.sin_addr.s_addr = INADDR_ANY;
-    server.server_addr.sin_port = htons(port);
-    
-    if (bind(
-        server.server_fd,
-        (struct sockaddr *)&server.server_addr,
-        sizeof(server.server_addr)) < 0) {
-        perror("ERROR: bind failed\n");
-        close(server.server_fd);
-        return NULL;
-    }
-
-    if (listen(server.server_fd, 10) < 0) {
-        perror("ERROR: listen failed\n");
-        close(server.server_fd);
-        return NULL;
-    }
-
-    printf("INFO: server successfully started on port %d\n", port);
-    return &server;
-}
-
-void start(struct Server* server) {
+void start() {
     while (1) {
         struct sockaddr_in client_addr;
         socklen_t client_addr_len = sizeof(client_addr);
         int *clientfd = (int*)malloc(sizeof(int));
         if (!clientfd) continue;
 
-        if ((*clientfd = accept(server->server_fd,
+        if ((*clientfd = accept(server.server_fd,
                             (struct sockaddr *)&client_addr,
                             &client_addr_len)) < 0 ) {
             perror("ERROR: accept failed\n");
@@ -78,11 +40,38 @@ void start(struct Server* server) {
 }
 
 int main(void) {
-    struct Server* server = init(PORT);
-    if (!server) {
-        printf("ERROR: server initializing failed\n");
+   if ((server.server_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+        perror("ERROR: socket failed\n");
         return 1;
     }
+    
+    int opt = 1;
+    if (setsockopt(server.server_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt)) < 0) {
+        perror("ERROR: setsockopt failed\n");
+        close(server.server_fd);
+        return 1;
+    }
+
+    server.server_addr.sin_family = AF_INET;
+    server.server_addr.sin_addr.s_addr = INADDR_ANY;
+    server.server_addr.sin_port = htons(PORT);
+    
+    if (bind(
+        server.server_fd,
+        (struct sockaddr *)&server.server_addr,
+        sizeof(server.server_addr)) < 0) {
+        perror("ERROR: bind failed\n");
+        close(server.server_fd);
+        return 1;
+    }
+
+    if (listen(server.server_fd, 10) < 0) {
+        perror("ERROR: listen failed\n");
+        close(server.server_fd);
+        return 1;
+    }
+
+    printf("INFO: server successfully started on port %d\n", PORT);
 
     start(server);
     
